0c2b24458976d8838ad92ecfd8c9a1f53f5634eb
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 23:49:07 -0700

    build: Move to new qcom HAL directory layout
    All CAF forked HALs now reside in hardware/qcom-caf/
    
    Change-Id: I9f84cebf021418c0b402bc3186f37643e63afb24
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -85,9 +85,8 @@ QCOM_HARDWARE_VARIANT := $(OVERRIDE_QCOM_HARDWARE_VARIANT)
 endif
 
 PRODUCT_SOONG_NAMESPACES += \
-    hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT) \
-    hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT) \
-    hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT)
+    hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)
+endif
 
 # QCOM HW crypto
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
