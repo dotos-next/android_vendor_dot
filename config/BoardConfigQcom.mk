dc215758ec0f5698d92d57e375c9a4751799982b
Michael Bestas <mkbestas@lineageos.org>
2019-09-25 21:10:29 +0300

    build: Remove TARGET_CRYPTFS_HW_PATH
    * Not used since 16.0 where vold/cryptfs_hw switched to bp
    
    Change-Id: I649402d9a3b94df8810d577147e09a51833192a0
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -87,8 +87,3 @@ endif
 PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)
 endif
-
-# QCOM HW crypto
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
-endif
